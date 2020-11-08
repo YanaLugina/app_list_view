e7163cd497abd6432d5f1baf1b9a68cf550ce619
YanaLugina <yana4961@gmail.com>
2020-11-09 00:14:39 +1000

    vertical: changed icons color and add functions on Tap

--- lib/main.dart
+++ lib/main.dart
@@ -36,24 +36,28 @@ Widget _myListView() {
     // shrinkWrap: true, // very heavy, need many recalculation actions, do not use this
     // scrollDirection: Axis.horizontal,
     // itemExtent: 300,  // GOOD, need to use, light for core ! this set max width for children when Axis.horizontal, or max height when Axis.vertical
+    // reverse: true,
     children: <Widget>[
       ListTile(
         title:Text('Sun'),
         subtitle: Text('Today Clear'),
-        leading: Icon(Icons.wb_sunny),
+        leading: Icon(Icons.wb_sunny, color: Colors.orangeAccent,),
         trailing: Icon(Icons.keyboard_arrow_right),
+        onTap:() => {},
       ),
       ListTile(
         title:Text('Cloudy'),
         subtitle: Text('Today Cloudy'),
-        leading: Icon(Icons.wb_cloudy),
+        leading: Icon(Icons.wb_cloudy, color: Colors.blue.shade300),
         trailing: Icon(Icons.keyboard_arrow_right),
+        onTap:() => {},
       ),
       ListTile(
         title:Text('Snow'),
         subtitle: Text('Today Snow'),
-        leading: Icon(Icons.ac_unit),
+        leading: Icon(Icons.ac_unit, color: Colors.grey),
         trailing: Icon(Icons.keyboard_arrow_right),
+        onTap:() => {},
       ),
     ],
   );
