1253c0e77f6256437fdbd75f5763215df42fab20
YanaLugina <yana4961@gmail.com>
2020-11-09 00:04:49 +1000

    vertical: changed widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -16,15 +16,15 @@ void main() {
     await tester.pumpWidget(MyApp());
 
     // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.text('Sun'), findsOneWidget);
+    expect(find.text('Not A Sun'), findsNothing);
 
     // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    await tester.tap(find.byIcon(Icons.wb_sunny));
     await tester.pump();
 
     // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('Not A Sun'), findsNothing);
+    expect(find.text('Sun'), findsOneWidget);
   });
 }
